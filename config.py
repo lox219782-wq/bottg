39830eb99ea50e867b131d69f0fc067a020cafde
lox219782-wq <lox219782@gmail.com>
2026-05-25 17:38:34 +0200

    Update config.py

--- config.py
+++ config.py
@@ -3,17 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# Читаем секреты
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_ID = os.getenv("TELEGRAM_API_ID")
-API_HASH = os.getenv("TELEGRAM_API_HASH")
 
-# Превращаем строку с ID админов (например, "123,456") в список чисел
 raw_admins = os.getenv("ADMIN_IDS", "")
 ADMIN_IDS = [int(x.strip()) for x in raw_admins.split(",") if x.strip().isdigit()]
 
-# Проверка, что критические данные на месте
 if not BOT_TOKEN:
-    raise ValueError("Критическая ошибка: Токен бота (BOT_TOKEN) не найден в секретах!")
+    raise ValueError("Критическая ошибка: Токен бота (BOT_TOKEN) не найден!")
 if not ADMIN_IDS:
-    raise ValueError("Критическая ошибка: Список ADMIN_IDS пуст или заполнен неверно!")
+    raise ValueError("Критическая ошибка: Список ADMIN_IDS пуст!")
