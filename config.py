b7eec231372137b5f48eb044cc62331f8d3c698d
lox219782-wq <lox219782@gmail.com>
2026-05-25 18:43:12 +0200

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,14 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 raw_admins = os.getenv("ADMIN_IDS", "")
 ADMIN_IDS = [int(x.strip()) for x in raw_admins.split(",") if x.strip().isdigit()]
 
+# Добавь эти строки:
+SESSIONS_DIR = "sessions"
+UPLOADS_DIR = "uploads"
+
+# Создаем папки, если их нет
+os.makedirs(SESSIONS_DIR, exist_ok=True)
+os.makedirs(UPLOADS_DIR, exist_ok=True)
+
 if not BOT_TOKEN:
     raise ValueError("Критическая ошибка: Токен бота (BOT_TOKEN) не найден!")
 if not ADMIN_IDS:
